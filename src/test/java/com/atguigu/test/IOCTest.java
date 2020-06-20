ca0a1d64dbcc3828bbcc11ad5404bf18b798c2e1
JackyLiuXiang <904045086@qq.com>
2020-06-20 11:35:20 +0800

    Spring_annotation first commit

---
+++ src/test/java/com/atguigu/test/IOCTest.java
@@ -0,0 +1,36 @@
+package com.atguigu.test;
+
+import com.atguigu.config.MainConfig;
+import com.atguigu.config.MainConfig2;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Jacky
+ * @create 2020-06-08 9:11
+ */
+public class IOCTest {
+
+    @Test
+    public void test01(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : definitionNames) {
+            System.out.println(name);
+        }
+    }
+
+    @Test
+    public void test02(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
+//		String[] definitionNames = applicationContext.getBeanDefinitionNames();
+//		for (String name : definitionNames) {
+//			System.out.println(name);
+//		}
+//
+        System.out.println("ioc容器创建完成....");
+        Object bean = applicationContext.getBean("person");
+        Object bean2 = applicationContext.getBean("person");
+        System.out.println(bean == bean2);
+    }
+}
